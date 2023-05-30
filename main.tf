9255255d7ae95a44e23277e69cd343f6a75d0a59
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-05-29 21:33:33 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.27"
+        }
+    }
+    required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "sa-east-1"
+}
+
+resource "aws_instance" "app_server" {
+    ami =           "ami-0af6e9042ea5a4e3e"
+    instance_type=  "t2.micro"
+
+    tags = {
+        Name = "Primeira Instancia"
+    }
+}
