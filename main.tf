5aedaf57df514200487f17523264111d3a0a38f8
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-06-01 11:31:48 -0300

    Adiciona user_data

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
     ami =           "ami-0af6e9042ea5a4e3e"
     instance_type=  "t2.micro"
     key_name = "iac-alura"
+    user_data = <<-EOF
+                   #!/bin/bash 
+                   cd
+                   echo "<h1> Hello World com Terraform </h1>" > index.html
+                   nohup busybox httpd -f -p 8080 &
+                   EOF
     tags = {
-        Name = "Primeira Instancia"
+        Name = "ec2_terraform_AWS"
     }
 }
